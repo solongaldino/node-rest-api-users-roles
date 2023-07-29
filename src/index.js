47762f89472407ca3533ee54f9f858dbed8c8933
Solon Galdino <solonfisicaufpb@gmail.com>
2023-07-28 23:07:07 -0300

    FEAT: config app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(routes);
+
+const serverPort = 3333;
+
+app.listen(serverPort, () => console.info(`Server is running on PORT ${serverPort}`));
