d4f3f2bf97af33eaa5edfeada16af48cae1ba60c
Solon Galdino <solonfisicaufpb@gmail.com>
2023-08-02 15:48:12 -0300

    FEAT: save roles in redis loggedUser

--- src/middlewares/express/authHandle.js
+++ src/middlewares/express/authHandle.js
@@ -41,6 +41,8 @@ async function getLoggedUserById(id) {
       loggedUserData,
       AuthJwt.expiresJwtAccessTokenInSeconds
     );
+
+    return loggedUserData;
   }
 
   return loggedUserData[0];
